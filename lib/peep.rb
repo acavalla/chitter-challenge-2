8c4dae0da59c51fd525a2888d74daae282b8c09f
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-05-11 13:10:36 +0100

    Passing unit test of hardcoded .all method

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,4 +2,12 @@ class Peep
   def initialize(text)
 
   end
+
+  def self.all
+    [
+      'Hi! First Peep',
+      'Way better than Twitter',
+      'Covfefe'
+    ]
+  end
 end
--- spec/units/peep_spec.rb
+++ spec/units/peep_spec.rb
@@ -1,10 +1,17 @@
 require './lib/peep.rb'
 
 describe Peep do
-  peep = Peep.new('Hi!')
+
   describe '.new' do
     it 'makes a new peep' do
+      peep = Peep.new('Hi!')
       expect(peep).to be_a(Peep)
+    end
+  end
+
+  describe '.all' do
+    it 'lists all Peeps' do
+      expect(Peep.all.length).to eq 3
     end
   end
 end
