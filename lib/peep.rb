fc8511f52e562a02cb594bc55ac7c5e53f61889f
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-05-12 13:00:13 +0100

    Lists time alongside Peep

--- lib/peep.rb
+++ lib/peep.rb
@@ -7,7 +7,7 @@ class Peep
     con = PG.connect :dbname => 'chitter', :user => 'annie'
     result = con.exec "SELECT * FROM peeps;"
     result.map do |row|
-      row['text']
+      row['text'] + " " + row['created_at']
     end
   end
 end
--- spec/units/peep_spec.rb
+++ spec/units/peep_spec.rb
@@ -12,9 +12,10 @@ describe Peep do
   describe '.all' do
     it 'lists all Peeps' do
       peeps = Peep.all
-      expect(peeps).to include 'Covfefe'
-      expect(peeps).to include 'Very stable genius'
-      expect(peeps).to include 'First Peep :)'
+      expect(peeps[0]).to include 'Covfefe'
+      expect(peeps[1]).to include '2021-05-12 11:59:45'
+      expect(peeps[2]).to include 'First Peep :)'
+      expect(peeps[3]).to include 'Very stable genius'
     end
   end
 end
