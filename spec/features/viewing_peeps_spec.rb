69b4bd95ceeb78e0de5fa8188f91ab57fab29745
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-05-12 12:54:56 +0100

    Testing for time showing

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -7,5 +7,6 @@ feature 'Viewing peeps' do
   scenario 'visiting the Peeps page' do
     visit '/peeps'
     expect(page).to have_content('Covfefe')
+    expect(page).to have_content('2021-05-12 11:59:45')
   end
 end
