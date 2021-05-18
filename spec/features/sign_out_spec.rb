882d575e279cb01ee9eaff13eb86a9fc0551ae45
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-05-18 17:04:26 +0100

    Added web helper to sign in, added conditional sign out functionality and redirect

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -1,6 +1,13 @@
+require_relative 'web_helpers'
+
 feature 'Signing out' do
   scenario 'User on peeps page clicks sign out' do
+
     visit '/peeps'
+    expect(page).not_to have_content("Sign out")
+    create_user_and_sign_in
+    expect(page).to have_content("Sign out")
     click_link("Sign out")
+    expect(current_path).to eq ('/session/new')
   end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,3 +1,11 @@
 def sign_up
   User.create(email: "acav@gmail.com", password: "password")
 end
+
+def create_user_and_sign_in
+  visit('/session/new')
+  sign_up
+  fill_in('email', with: "acav@gmail.com")
+  fill_in('password', with: "password")
+  click_button('Sign me in!')
+end
