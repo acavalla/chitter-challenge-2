ec1aaf79508f9d8644e98fc8e64a222174157493
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-05-19 14:11:53 +0100

    Can only peep if signed in

--- spec/features/creating_peeps_spec.rb
+++ spec/features/creating_peeps_spec.rb
@@ -1,5 +1,13 @@
+require_relative 'web_helpers'
+
 feature 'Creating peeps' do
+  scenario 'cannot peep unless signed in' do
+    visit '/peeps'
+    expect(page).not_to have_content 'What would you like to peep?'
+  end
+
   scenario 'visiting the Peeps page' do
+    create_user_and_sign_in
     visit '/peeps'
     expect(page).to have_content('What would you like to peep?')
     fill_in('peep', with: 'hiya')
